74ca187ff604c0ca2ff984b8c9261a3e68714d2f
Natebuilder <d272226@edu.curio.nl>
2024-03-22 13:39:51 +0100

    bruh 300+ changes?

---
+++ Data/Egg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSimWiki.Data
+{
+    public class Egg
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Pet> Pets { get; set; } = new List<Pet>();
+    }
+}
---
+++ Data/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSimWiki.Data
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int EggId { get; set; }
+        public Egg Egg { get; set; }
+    }
+}
